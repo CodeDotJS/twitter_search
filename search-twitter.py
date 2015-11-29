ce3e83e3004b1a590e82eb18d52e9050daf1445d
Rishi Giri <rushforlinux@gmail.com>
2015-11-30 04:03:02 +0530

    Update search-twitter.py

--- search-twitter.py
+++ search-twitter.py
@@ -2,10 +2,10 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 
 x=1
+s = raw_input("Enter a digit: ")
+print "Started . . . "
 
-while(x<=n)
+while(x<=s):
     print BeautifulSoup(urllib2.urlopen("https://www.twitter.com/"+str(x))).title.string
     x+=1
     
-
-
